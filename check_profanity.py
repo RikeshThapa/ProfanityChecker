91b56e8eb4f41e75c50a7d83c94e6aef0662c4d3
Rikesh Thapa <rthapa@u.rochester.edu>
2017-01-12 09:52:07 -0800

    finishing basic checker

--- check_profanity.py
+++ check_profanity.py
@@ -1,6 +1,15 @@
+import urllib
+
 def read_text():
     quotes = open('movie_quotes.txt')
     read = quotes.read()
     quotes.close()
+    check_profanity(read)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
 
 read_text()
